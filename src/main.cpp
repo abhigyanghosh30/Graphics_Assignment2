c403ae1eeb687b271eac8cf06acebe2b23a825c4
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2019-02-04 02:24:30 +0530

    increased window size to 1080x1080

--- src/main.cpp
+++ src/main.cpp
@@ -135,8 +135,8 @@ void initGL(GLFWwindow *window, int width, int height) {
 
 int main(int argc, char **argv) {
     srand(time(0));
-    int width  = 600;
-    int height = 600;
+    int width  = 1080;
+    int height = 1080;
 
     window = initGLFW(width, height);
 
